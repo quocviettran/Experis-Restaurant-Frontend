974726527bcbc572c04760791f853e0dc5215e89
Mathias Dahl Andersen <mattis417@hotmail.com>
2019-03-19 11:06:37 +0100

    yes

--- app.js
+++ app.js
@@ -3,17 +3,15 @@ const path = require('path');
 // Set up the express app
 const app = express();
 // get all todos
-app.get('/api/v1/todos', (req, res) => {
-    res.status(200).send({
-        success: 'true',
-        message: 'todos retrieved successfully',
-        todos: db
-    })
-});
+const user = require("./routes/userController");
+const review = require("./routes/reviewController");
+const resturant = require("./routes/resturantController");
+
+app.use("/", user);
+app.use("/", review);
+app.use("/", resturant);
+
 const PORT = 3000;
-
-app.get("/", (req, res) => res.send("Hello World!"));
-
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`)
 });
