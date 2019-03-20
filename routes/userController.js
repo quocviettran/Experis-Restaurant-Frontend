860af37f3610d9fe74973a8bb0b394169bbec484
Quoc Viet N. Tran <quocviettran95@gmail.com>
2019-03-20 10:43:46 +0100

    Update users

--- routes/userController.js
+++ routes/userController.js
@@ -1,18 +1,18 @@
 const r = require("express").Router();
 const conn = require('./db');
-
+const now = new Date().toISOString();
 
 // User
 r.get("/user/:id", (req, res) => {
     conn.query(`SELECT * from USERS where user_id=${req.params.id}`, (err, result) => {
-        res.json(result);
+        res.json(result.rows);
     });
 });
 
 // All Users
 r.get("/user/", (req, res) => {
     conn.query(`SELECT * from USERS`, (err, result) => {
-        res.json(result);
+        res.json(result.rows);
     });
 });
 
@@ -20,17 +20,16 @@ r.get("/user/", (req, res) => {
 // Create user
 r.post("/user/create", (req, res) => {
     conn.query(
-      `INSERT INTO USERS (username, password, email,role, created_at,updated_at,active)`
-        + `VALUES ('${req.body.username}' , '${req.body.password}','${req.body.email}',${req.body.role},CURRENT_DATE,CURRENT_DATE,${req.body.active});`,
+      `INSERT INTO USERS (username, password, email,role, created_at,updated_at,active) VALUES ('${req.body.username}','${req.body.password}','${req.body.email}',${req.body.role},'${now}','${now}',1);`,
         (err,result)=>{
-            res.end('Successfully created');
+            res.send('Successfully created');
         });
 });
 
 // Update user
 r.post("/user/update", (req, res) => {
-    conn.query(`UPDATE USERS set username = 'HALLO' WHERE user_id = ${req.body.user_id}`, (err,result) =>{
-        res.send("SUCCESS");
+    conn.query(`UPDATE USERS set username = '${req.body.username}', password = '${req.body.password}', email = '${req.body.email}' , updated_at = ${now} WHERE user_id = ${req.body.user_id}`, (err,result) =>{
+        res.send("Successfully updated");
     });
 });
 
