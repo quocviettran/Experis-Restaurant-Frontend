d726f46f2cd73c96c5f9e3a526dfcd1f1fc15255
HeleneHarmens <helene.harmens@gmail.com>
2019-03-20 11:00:42 +0100

    hei

--- routes/resturantController.js
+++ routes/resturantController.js
@@ -1,24 +1,76 @@
 const r = require("express").Router();
 const conn = require("./db");
 
-// Resturant
+
+// SHOW Resturant with id
 r.get("/restaurant/:id", (req, res) => {
     conn.query(
-      `SELECT * from restaurant where restaurant_id=${req.params.id}`,
-      (err, result) => {
+      `SELECT * FROM restaurant WHERE restaurant_id=${req.params.id}`, 
+    (err, result) => {
         res.json(result);
-      }
-    );
+      });
 });
 
-// Create Restaurant
+// SHOW ALL Restaurants
+r.get("/restaurant/", (req, res) => {
+  conn.query(
+    `SELECT * FROM restaurant`, 
+    (err, result) => {
+      res.json(result);
+  });
+});
+
+// CREATE restaurant
 r.post("/restaurant/create", (req, res) => {
-    res.send("Created...");
+  const now = new Date().toISOString();
+  conn.query(
+    `INSERT INTO restaurant (user_id, name, address, category, description, created_at, updated_at, active)`
+      + `VALUES (${user_id}, '${req.body.name}','${req.body.address}','${req.body.category}',
+      '${req.body.description}', '${now}', '${now}', ${req.body.active});`,
+      (err,result)=>{
+          res.end('Successfully created restaurant');
+      });
 });
 
-// Update Resturant
+// UPDATE restaurant
 r.post("/restaurant/update", (req, res) => {
-    res.send("Updated...");
+  const now = new Date().toISOString();
+  conn.query(
+    `UPDATE restaurant 
+      SET restaurant_id = ${req.body.restaurant_id},
+      SET name = '${req.body.name}', 
+      SET description = '${req.body.description}',
+      SET category = '${req.body.category}',
+      SET updated_at = '${now}',
+      WHERE user_id = ${req.body.user_id}`,
+  (err,result) =>{
+    if (err) { 
+      res.send(err);
+    } else {
+      res.end('Successfully updated restaurant');
+    }    
+  });
 });
+
+// DEACTIVATE restaurant
+r.post("/restaurant/deactivate/:id", (req, res) => {
+  conn.query(
+    `UPDATE restaurant
+      SET active = 0 WHERE user_id =${req.body.id}`,
+  (err, result) =>{
+    res.end('Successfully deactivated restaurant');
+  });
+});
+
+// ACTIVATE restaurant
+r.post("/restaurant/deactivate/:id", (req, res) => {
+  conn.query(
+    `UPDATE restaurant
+      SET active = 1 WHERE user_id =${req.body.id}`,
+  (err, result) =>{
+    res.end('Successfully reactivated restaurant');
+  });
+});
+
 
 module.exports = r;
