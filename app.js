41715f9f500fdcf34accfb42017618bfbb4d9f55
Mathias Dahl Andersen <mattis417@hotmail.com>
2019-03-21 09:37:26 +0100

    cors

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const path = require('path');
 const app = express();
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // Get all todos
 const user = require("./routes/userController");
 const review = require("./routes/reviewController");
